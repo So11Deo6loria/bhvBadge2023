62ff0838dc331b72e4fe3a7d7e585db5d829c68d
patelpratham11 <ppatel011102@gmail.com>
2023-08-04 12:46:22 -0400

    re-did HR algorithm with IIR

--- firmware/hrMonitor.py
+++ firmware/hrMonitor.py
@@ -9,6 +9,12 @@ class HRMonitor:
   def __init__(self, i2cHandle, ledHandle):
     self.i2cHandle = i2cHandle
     self.ledHandle = ledHandle
+    self.b0 = 0.5
+    self.b1 = 0.1
+    self.a1 = 0.3
+    self.x_prev = 0
+    self.y_prev = 0
+
     self.setupSensor()
 
   def setupSensor(self):
@@ -28,44 +34,28 @@ class HRMonitor:
     self.sensor.set_pulse_amplitude_it(pulseAmp)
     self.sensor.set_active_leds_amplitude(ledAmp)
 
-  def calculate_heart_rate(self, ir_data_array, sample_rate=100, ema_alpha=0.2, moving_average_length=5):
-    # Check if the input data is sufficient for moving average filtering
-    if len(ir_data_array) < moving_average_length:
-        return None
+  def filter(self, x_curr):
+    y_curr = self.b0*x_curr + self.b1*self.x_prev - self.a1*self.y_prev
+    self.x_prev = x_curr
+    self.y_prev = y_curr
+    return y_curr
 
-    # Apply a simple moving average filter to the raw IR data
-    ir_smoothed = []
-    for i in range(len(ir_data_array) - moving_average_length + 1):
-        ir_average = sum(ir_data_array[i:i + moving_average_length]) / moving_average_length
-        ir_smoothed.append(ir_average)
+  def find_peaks(self, ir_data, threshold):
+     peaks = []
+     for i in range(1, len(ir_data)-1):
+        if ir_data[i] > ir_data[i-1] and ir_data[i] > ir_data[i+1] and ir_data[i] > threshold:
+           peaks.append(i)
+     return peaks
 
-    # Apply an exponential moving average (EMA) filter to the smoothed IR data
-    ema = ir_smoothed[0]
-    filtered_ir_data = [ema]
-    for ir_data in ir_smoothed[1:]:
-        ema = ema_alpha * ir_data + (1 - ema_alpha) * ema
-        filtered_ir_data.append(ema)
+  def calculate_heart_rate(self, ir_data_array, sample_rate=100, ema_alpha=0.2, moving_average_length=3):
+    peak_values = self.find_peaks(ir_data_array, 750)
+    if(len(peak_values) >= 2):
+       time_interval = (peak_values[-1] - peak_values[0])/sample_rate
+       heart_rate = 60/time_interval
+       return heart_rate
+    return None
 
-    # Find peaks (local maxima) in the filtered IR signal
-    peaks = []
-    for i in range(1, len(filtered_ir_data) - 1):
-        if filtered_ir_data[i] > filtered_ir_data[i - 1] and filtered_ir_data[i] > filtered_ir_data[i + 1]:
-            peaks.append(i)
-
-    # Calculate time between consecutive peaks and estimate heart rate in BPM
-    if len(peaks) >= 2:
-        time_between_peaks = (peaks[-1] - peaks[0]) / sample_rate  # Time in seconds between first and last peak
-        heart_rate_bpm = 60 / time_between_peaks
-
-        # Range check for BPM values
-        min_bpm = 40
-        max_bpm = 200
-        if min_bpm <= heart_rate_bpm <= max_bpm:
-            return heart_rate_bpm
-        else:
-            return None
-    else:
-        return None
+  
 
   def hrRunLoop(self):        
     # Check if the Max30102 is present
@@ -89,12 +79,15 @@ class HRMonitor:
     
     while True:
         # Read data from the Max30102
-        #red_data, ir_data = read_max30102_data(i2c)
+        # red_data, ir_data = read_max30102_data(i2c)
         self.sensor.check()
         if( self.sensor.available() ):            
             red_data = self.sensor.pop_red_from_storage()
             ir_data = self.sensor.pop_ir_from_storage()
-            ir_data_array.append(ir_data)
+            # print("RAW IR: ", ir_data)
+            filtered = self.filter(ir_data)
+            # print("Filtered: ", filtered)
+            ir_data_array.append(filtered)
 
             # Limit the size of the array to prevent excessive memory usage
             max_array_size = 100
@@ -103,16 +96,13 @@ class HRMonitor:
 
             # Calculate heart rate and print it to the console
             heart_rate = self.calculate_heart_rate(ir_data_array)
-            if heart_rate is not None:
-                if bpm_ema is None:
-                    bpm_ema = heart_rate
-                else:
-                    bpm_ema = bpm_ema_alpha * heart_rate + (1 - bpm_ema_alpha) * bpm_ema
-                print("Filtered Heart Rate:", bpm_ema, "BPM")
+            if heart_rate is not None and heart_rate > 40 and heart_rate < 200:
+                print("Filtered Heart Rate:", round(heart_rate), "BPM")
+                pass
             else:
                 print("Heart Rate calculation failed. Not enough peaks detected.")
 
             # Wait a moment before taking the next reading
-            time.sleep(0.1)
+            time.sleep(0.01)
         else:
             time.sleep(0.05)
